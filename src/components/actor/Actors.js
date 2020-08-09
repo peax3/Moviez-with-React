74e42c0903e5fc4cfaa1ecb7f7ce10a3b59a16bf
peax3 <peaceadjerese@gmail.com>
2020-08-09 06:40:46 +0100

    add showAll functionality to actors component

--- src/components/actor/Actors.js
+++ src/components/actor/Actors.js
@@ -1,25 +1,47 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 
 import ActorCard from "../card/ActorCard";
 
 const Actors = ({ actors }) => {
+  const [showAllActors, setShowAllActors] = useState(false);
+
+  const handleClick = (e) => {
+    setShowAllActors(e.target.checked);
+  };
+
+  let actorsToShow;
+
   if (actors === null || actors === undefined) {
     return null;
   } else if (actors.length === 0) {
     return null;
-  } else {
-    return (
-      <div>
-        <h2>Actors</h2>
-
-        <div className="actors">
-          {actors.map((actor) => (
-            <ActorCard key={actor.id} actor={actor} />
-          ))}
-        </div>
-      </div>
-    );
   }
+
+  if (actors.length <= 10) {
+    actorsToShow = actors;
+  } else if (actors.length > 10 && !showAllActors) {
+    actorsToShow = actors.slice(0, 10);
+  } else if (actors.length > 10 && showAllActors) {
+    actorsToShow = actors;
+  }
+
+  return (
+    <div className="actors-container">
+      <h2>Actors</h2>
+      {actors.length > 10 && (
+        <label htmlFor="show Actors">
+          showAllActors
+          <input type="checkbox" onClick={handleClick} />
+        </label>
+      )}
+
+      <div className="actors">
+        {actorsToShow.map((actor) => (
+          <ActorCard key={actor.id} actor={actor} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Actors;
